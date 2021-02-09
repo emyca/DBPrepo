d3564cd277bca9653bd51fb120fa11aa412e77b8
emyca <evmyca@gmail.com>
2021-02-09 02:52:46 +0200

    Fix test

--- app/src/androidTest/java/ua/kh/em/dbprepo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ua/kh/em/dbprepo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("em.kh.ua.roomprepo", appContext.packageName)
+        assertEquals("ua.kh.em.dbprepo", appContext.packageName)
     }
 }
